6daace5aa8f98ea99b390d5f88376b7b37bc7c85
qm-naushad <102563493+qm-naushad@users.noreply.github.com>
2023-10-08 23:28:18 +0400

    sdf

---
+++ scriptparallax.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    const parallaxSections = document.querySelectorAll('.parallax-section');
+    parallaxSections.forEach(section => {
+        const speed = 0.5;
+        const offset = window.pageYOffset;
+        section.style.backgroundPositionY = (offset - section.offsetTop) * speed + 'px';
+    });
+});
